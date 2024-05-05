92aaecc548c257254bea7de608c90ac27b0f8793
Oliver Ni <oliver.ni@gmail.com>
2024-05-05 05:29:23 -0700

    feat: tailwindcss typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,27 @@ module.exports = {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
         serif: ["EB Garamond", ...defaultTheme.fontFamily.serif],
       },
+      typography: ({ theme }) => ({
+        DEFAULT: {
+          css: {
+            a: {
+              textDecoration: "none",
+              fontWeight: "medium",
+              // color: theme("colors.blue.500"),
+              transitionProperty: theme("transitionProperty.shadow"),
+              transitionTimingFunction: theme(
+                "transitionTimingFunction.in-out"
+              ),
+              transitionDuration: theme("transitionDuration.150"),
+              boxShadow: `inset 0 -0.4em ${theme("colors.gray.200")}`,
+              "&:hover": {
+                boxShadow: `inset 0 -0.6em ${theme("colors.gray.300")}`,
+              },
+            },
+          },
+        },
+      }),
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
