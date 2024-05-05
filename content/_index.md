cc29eb3d391a2f0d057895cf048f496a91e588a2
Oliver Ni <oliver.ni@gmail.com>
2024-05-05 02:21:16 -0700

    feat: add basic template

---
+++ content/_index.md
@@ -0,0 +1,5 @@
++++
+title = "Oliver Ni"
++++
+
+Hi! I'm a software engineer from San Jose, CA.
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./**/*.{html,md}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+        serif: ["EB Garamond", ...defaultTheme.fontFamily.serif],
+      },
+    },
   },
   plugins: [],
 };
